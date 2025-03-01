89c70451a1007c80ef6b275c7db14c47b92c0b8d
Raj Bodhak <rajlaptop2004@gmail.com>
2025-03-01 17:19:01 +0530

    Add user routes

---
+++ DevNet/Server/routers/user.router.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+    register,
+    login,
+    logout,
+    getuser,
+    getSuggestedUsers,
+    editUser,
+    followOrUnfollow
+} from "../controllers/user.controller.ts";
+import isAuthenticated from "../middlewares/isAuthenticated.ts";
+import upload from "../utils/multer.ts";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.get("/user/:id", isAuthenticated, getuser);
+router.get("/suggested-users", isAuthenticated, getSuggestedUsers);
+router.put("/profile/edit", isAuthenticated, upload.single("profilePicture"), editUser);
+router.post("/follow/:followId", isAuthenticated, followOrUnfollow);
+
+export default router
