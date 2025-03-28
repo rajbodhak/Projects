8bc2a7793b679200f4456b6289851b4e6ae085ca
Raj Bodhak <rajlaptop2004@gmail.com>
2025-03-28 23:42:24 +0530

    Start implementing Search Section

--- DevNet/Client/src/Pages/Search.tsx
+++ DevNet/Client/src/Pages/Search.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { SearchIcon } from 'lucide-react'
 
 const Search = () => {
     return (
-        <div>
-            This is Search Page
+        <div className='flex items-center justify-center flex-col'>
+            <h1 className='text-center text-xl my-4 font-black'>Explore </h1>
+            <div className='flex gap-1 items-center rounded-lg shadow-xl border border-gray-500 px-2'>
+                <input
+                    type="text"
+                    placeholder='search here'
+                    name='search-bar'
+                    className='px-4 py-3 outline-none'
+                />
+                <SearchIcon className='cursor-pointer hover:text-amber-500' />
+            </div>
         </div>
     )
 }
